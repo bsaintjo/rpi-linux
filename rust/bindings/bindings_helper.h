dfc2c1dcbdc44b83456deb9e0840dec114472ca5
Fabien Parent <fabien.parent@linaro.org>
2023-11-28 19:22:46 -0800

    rust: add abstraction for regmap
    This regmap abstraction is implemented using only the regmap_field
    APIs. This abstraction tries to bring some type-safety features and
    ease of use by enhancing the regmap API through extensive macro use
    to generate code.
    The abstraction is bringing only a small subset of all the features
    provided by regmap by only supporting the most vital field from
    `struct regmap_config`.
    
    This abstraction is used by the Regulator abstraction as well as the
    following driver:
    https://github.com/Fabo/linux/blob/b4/ncv6336/drivers/regulator/ncv6336_regulator.rs
    
    Signed-off-by: Fabien Parent <fabien.parent@linaro.org>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -66,6 +66,7 @@
 #include <linux/poll.h>
 #include <linux/property.h>
 #include <linux/refcount.h>
+#include <linux/regmap.h>
 #include <linux/sched.h>
 #include <linux/security.h>
 #include <linux/slab.h>
