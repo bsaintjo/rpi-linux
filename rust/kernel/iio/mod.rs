69614ec1bd57e45fd6a2868feb8b461707cbf0ba
Brandon T. P. Saint-John <saint-john@lbl.gov>
2025-07-07 23:14:10 +0000

    implement vtable adapter

--- rust/kernel/iio/mod.rs
+++ rust/kernel/iio/mod.rs
@@ -1,65 +1,108 @@
 use core::{marker::PhantomData, ptr::{null, null_mut, NonNull}};
 
-use bindings::{__iio_device_register, iio_device_alloc};
-
 use crate::{device, driver, error::to_result, str::CStr, types::Opaque, ThisModule};
 use crate::prelude::*;
+use core::mem::MaybeUninit;
+use crate::error::VTABLE_DEFAULT_ERROR;
 
-struct Adapter<T>(T);
+// struct Adapter<T>(T);
 
-unsafe impl<T> driver::RegistrationOps for Adapter<T> {
-    type RegType = bindings::iio_dev;
+// unsafe impl<T> driver::RegistrationOps for Adapter<T> {
+//     type RegType = bindings::iio_dev;
 
-    unsafe fn register(
-        indio_dev: &Opaque<Self::RegType>,
-        name: &'static CStr,
-        module: &'static ThisModule,
-    ) -> Result {
+//     unsafe fn register(
+//         indio_dev: &Opaque<Self::RegType>,
+//         name: &'static CStr,
+//         module: &'static ThisModule,
+//     ) -> Result {
 
-        // let indio_dev = unsafe { iio_device_alloc(null_mut::<device>(), 0)};
-        to_result(unsafe {
-            __iio_device_register(indio_dev.get(), module.as_ptr())
-        })
-    }
+//         // let indio_dev = unsafe { iio_device_alloc(null_mut::<device>(), 0)};
+//         to_result(unsafe {
+//             __iio_device_register(indio_dev.get(), module.as_ptr())
+//         })
+//     }
 
-    unsafe fn unregister(indio_dev: &Opaque<Self::RegType>) {
-        unsafe { bindings::iio_device_unregister(indio_dev.get()) };
-    }
-}
-
-impl<T> Adapter<T> {
-    extern "C" fn probe_callback() {
-
-    }
-    extern "C" fn remove_callback() {
-
-    }
-}
-
-pub struct SWDevice;
-
-pub trait SWDriver {
-    fn probe(name: &'static CStr) -> Pin<KBox<Self>>;
-}
+//     unsafe fn unregister(indio_dev: &Opaque<Self::RegType>) {
+//         unsafe { bindings::iio_device_unregister(indio_dev.get()) };
+//     }
+// }
 
 pub struct IioDevice<T> {
     indio_dev: NonNull<bindings::iio_dev>,
     _priv: PhantomData<T>,
 }
 
+// TODO Safety
+unsafe impl<T: Send + Sync > Send for IioDevice<T> {}
+unsafe impl<T: Send + Sync > Sync for IioDevice<T> {}
+
+// TODO Sealed for now, figure out if necessary
+impl<T: Send + Sync> crate::private::Sealed for IioDevice<T> {}
+
 impl<T> IioDevice<T> {
-    fn probe(dev: &crate::device::Device, module: &'static ThisModule) {
+    pub fn register(dev: &crate::device::Device, module: &'static ThisModule) -> Result<Self> {
         let sizeof_priv = core::mem::size_of::<T>() as ffi::c_int;
+        // TODO Safety
         let indio_dev = unsafe { bindings::iio_device_alloc(dev.as_raw(), sizeof_priv)};
+
         // unsafe { bindings::iio_priv }
+
+        // TODO Safety
         let ret = unsafe { bindings::__devm_iio_device_register(dev.as_raw(), indio_dev, module.as_ptr())};
+
+        Ok(Self {
+            indio_dev: NonNull::new(indio_dev).ok_or(EINVAL)?,
+            _priv: PhantomData,
+        })
     }
 }
 #[vtable]
 pub trait Driver<const N: usize> {
     const CHANNELS: [IioChanSpec; N];
-    fn read_raw<T>(indio_dev: &mut IioDevice<T>, _channel: &IioChanSpec, _fst: u32, _snd: u32, _mask: u32);
-    fn write_raw<T>(indio_dev: &mut IioDevice<T>, _channel: &IioChanSpec, _fst: u32, _snd: u32, _mask: u32);
+    fn read_raw<T>(
+        indio_dev: &mut IioDevice<T>,
+        _channel: &IioChanSpec,
+        _fst: i32,
+        _snd: i32, 
+        _mask: isize)
+    {
+        build_error!(VTABLE_DEFAULT_ERROR)
+    }
+
+    fn write_raw<T>(indio_dev: &mut IioDevice<T>, _channel: &IioChanSpec, _fst: i32, _snd: i32, _mask: isize);
+}
+
+pub struct IioVTableAdapter<const N: usize, T: Driver<{ N }>>(PhantomData<T>);
+
+impl<const N: usize, T: Driver<{ N }>> IioVTableAdapter<N, T> {
+    unsafe extern "C" fn read_raw(
+        indio_dev: *mut bindings::iio_dev,
+        iio_chan_spec: *const bindings::iio_chan_spec,
+        val: *mut ffi::c_int,
+        val2: *mut ffi::c_int,
+        mask: isize
+    ) -> ffi::c_int{
+        todo!()
+    }
+    unsafe extern "C" fn write_raw(
+        indio_dev: *mut bindings::iio_dev,
+        iio_chan_spec: *const bindings::iio_chan_spec,
+        val: ffi::c_int,
+        val2: ffi::c_int,
+        mask: isize
+    ) -> ffi::c_int {
+        todo!()
+    }
+
+    const VTABLE: bindings::iio_info = bindings::iio_info {
+        read_raw: Some(Self::read_raw),
+        write_raw: if T::HAS_WRITE_RAW { Some(Self::write_raw) } else { None },
+        ..unsafe { MaybeUninit::zeroed().assume_init() }
+    };
+
+    const fn build() -> &'static bindings::iio_info {
+        &Self::VTABLE
+    }
 }
 
 
@@ -77,12 +120,4 @@ impl IioChanSpec {
     pub fn with_temp(mut self) -> Self {
         todo!()
     }
-}
-
-
-#[macro_export]
-macro_rules! module_iio_sw_device_driver {
-    ($($f:tt)*) => {
-        $crate::module_driver!(<T>, $crate::iio::Adapter<T>, { $($f)* });
-    };
 }
