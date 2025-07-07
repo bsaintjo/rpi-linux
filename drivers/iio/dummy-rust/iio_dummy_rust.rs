57b5718f6f0b7848ea1b731896ebb4d40f4e916e
Brandon Saint-John <saint-john@lbl.gov>
2025-07-07 13:58:08 +0000

    Rust Dummy IIO driver for building iio rust abstractions

---
+++ drivers/iio/dummy-rust/iio_dummy_rust.rs
@@ -0,0 +1,13 @@
+fn test() {}
+
+struct Dummy;
+
+// impl iio::Driver for Dummy {
+//     fn read_raw(indio_dev: &mut Device) {
+//         todo!()
+//     }
+
+//     fn write_raw(indio_dev: &mut Device) {
+//         todo!()
+//     }
+// }
