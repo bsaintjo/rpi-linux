685298013216c64db1e13f4fcb79922421e5dfac
Brandon Saint-John <saint-john@lbl.gov>
2025-07-08 05:05:08 +0000

    cm3232: Update to registration type

--- drivers/iio/light/cm3232-rust/cm3232_rust.rs
+++ drivers/iio/light/cm3232-rust/cm3232_rust.rs
@@ -77,10 +77,10 @@ impl iio::Driver for Cm3232 {
         iio::IioChanSpec,
     ];
 
-    fn read_raw<T>(indio_dev: &mut iio::IioDevice<T>, _channel: &iio::IioChanSpec, _val: i32, _val2: i32, _mask: isize) {
+    fn read_raw<T>(indio_dev: &mut iio::Registration<T>, _channel: &iio::IioChanSpec, _val: i32, _val2: i32, _mask: isize) {
         todo!()
     }
-    fn write_raw<T>(indio_dev: &mut iio::IioDevice<T>, _channel: &iio::IioChanSpec, _val: i32, _val2: i32, _mask: isize) {
+    fn write_raw<T>(indio_dev: &mut iio::Registration<T>, _channel: &iio::IioChanSpec, _val: i32, _val2: i32, _mask: isize) {
         todo!()
     }
 }
