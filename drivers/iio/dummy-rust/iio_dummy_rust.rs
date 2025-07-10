78ff10766919fb3540212b7dfa638a49989a04d7
Brandon Saint-John <saint-john@lbl.gov>
2025-07-10 06:43:55 +0000

    iio-dummy-rs: Expand dummy to single channel example with data

--- drivers/iio/dummy-rust/iio_dummy_rust.rs
+++ drivers/iio/dummy-rust/iio_dummy_rust.rs
@@ -1,77 +1,83 @@
-use kernel::faux;
-
-use kernel::c_str;
-use kernel::prelude::*;
-use kernel::sync::Mutex;
-use kernel::iio;
+//! Implementation of a dummy device driver for the industrial I/O subsystem in Rust
+//!
+//! The goal is to demonstrate the Rust abstractions
+use kernel::{c_str, faux, iio, prelude::*, sync::Mutex};
 
 module! {
-    type: IioDummyModule,
+    type: DummyModule,
     name: "rust_iio_dummy",
     authors: ["Brandon Saint-John"],
     description: "IIO dummy driver in Rust",
     license: "GPL",
 }
 
-struct IioDummyModule {
+struct DummyModule {
     _fdev: faux::Registration,
-    indio_dev: iio::Registration<EmptyState>,
+    _indio_dev: iio::Registration<DummyDevice>,
 }
 
-impl kernel::Module for IioDummyModule {
+impl kernel::Module for DummyModule {
     fn init(module: &'static ThisModule) -> Result<Self> {
         let _fdev = faux::Registration::new(c_str!("rust-iio-dummy-faux"), None)?;
         let options = iio::RegistrationOptions {
             name: c_str!("rust-iio-dummy"),
             mode: iio::Mode::Direct,
         };
-        let indio_dev: iio::Registration<EmptyState> = iio::Registration::new(_fdev.as_ref(), module, &options)?;
-        Ok(Self {
-            _fdev,
-            indio_dev,
-        })
+        let _indio_dev: iio::Registration<DummyDevice> =
+            iio::Registration::new(_fdev.as_ref(), module, &options)?;
+        Ok(Self { _fdev, _indio_dev })
     }
 }
 
-const DUMMY_CHANNELS: &'static [iio::Specification] = &[
-     iio::Specification::new(iio::ChannelType::Light), 
-];
+const DUMMY_CHANNELS: &'static [iio::Specification] =
+    &[iio::Specification::new(iio::ChannelType::Voltage)
+        .as_output()
+        .info_mask_separate(iio::channels::RAW)];
 
-struct EmptyState;
-
-#[vtable]
-impl iio::Driver for EmptyState {
-    type Ptr = Pin<KBox<Self>>;
-    const CHANNELS: &'static [iio::Specification] = DUMMY_CHANNELS;
+struct DummyState {
+    dac_val: i32,
 }
 
-
-struct IioDummyState {
-    dac_val: i32,
-    single_ended_adc_val: i32,
-    differential_adc_val: [i32; 2],
-    accel_val: i32,
-    accel_calibbias: i32,
-    activity_running: i32,
-    activity_walking: i32,
-    // accel_calib_scale
-    steps_enabled: i32,
-    steps: i32,
-    height: i32,
+impl Default for DummyState {
+    fn default() -> Self {
+        Self { dac_val: 0 }
+    }
 }
 
 #[pin_data]
-struct IioDummy {
+struct DummyDevice {
     #[pin]
-    st: Mutex<IioDummyState>,
+    st: Mutex<DummyState>,
 }
 
-// impl iio::Driver for Dummy {
-//     fn read_raw(indio_dev: &mut Device) {
-//         todo!()
-//     }
+#[vtable]
+impl iio::Driver for DummyDevice {
+    type Ptr = Pin<KBox<Self>>;
+    const CHANNELS: &'static [iio::Specification] = DUMMY_CHANNELS;
 
-//     fn write_raw(indio_dev: &mut Device) {
-//         todo!()
-//     }
-// }
+    fn read_raw(data: Pin<&Self>, spec: &iio::Specification) -> iio::SensorResult<i32> {
+        match spec.channel_type() {
+            iio::ChannelType::Voltage => {
+                let guard = data.st.lock();
+                iio::SensorResult::int(guard.dac_val)
+            }
+            _ => todo!(),
+        }
+    }
+
+    fn write_raw(data: Pin<&mut Self>, spec: &iio::Specification, value: i32) -> Result {
+        match spec.channel_type() {
+            iio::ChannelType::Voltage if spec.is_differential() => {
+                let mut guard = data.st.lock();
+                guard.dac_val = value;
+            }
+            _ => todo!(),
+        }
+        // Let-chains version
+        // if let iio::ChannelType::Voltage = spec.channel_type() && spec.is_differential() {
+        //     let guard = data.st.lock();
+        //     *guard.dac_val = value;
+        // }
+        Ok(())
+    }
+}
